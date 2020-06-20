0639d22321bd8df369faa25e5587063d3877001a
thatskevinjain <kevin@getfrapp.com>
2020-06-20 11:15:56 +0530

    Added support for listing blogs

--- js/markdown.js
+++ js/markdown.js
@@ -29,26 +29,12 @@ switch(pathname){
 }
 
 
-////////////////////
-//    FUNCTIONS   //
-////////////////////
+/////////////////////////////
+//    MARKDOWN FUNCTIONS   //
+/////////////////////////////
 async function fetchBlogData(data = {}) {
 
 	const url = 'http://139.59.62.18:2018/blog/fetch'
-
-	const response = await fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(data)
-	});
-	return response.json();
-}
-
-async function fetchAllBlog(data = {}) {
-
-	const url = 'http://139.59.62.18:2018/blog/fetchAll'
 
 	const response = await fetch(url, {
 		method: 'POST',
@@ -82,6 +68,9 @@ async function fetchBlog(){
 	const name = url.searchParams.get("name");
 
 	if(name && name.length > 0){
+		// Hide the LIST DIV Element //
+		document.getElementById("outer-list").style.display = "none"
+
 		populateBody(name)
 	}
 	else{
@@ -89,6 +78,58 @@ async function fetchBlog(){
 		document.getElementById("outer-markdown").style.display = "none"
 
 		// Fetch all blogs //
-		fetchAllBlog().then(console.log).catch(console.log)
+		createListItems()
 	}
+}
+
+/////////////////////////////
+//   LIST BLOG FUNCTIONS   //
+/////////////////////////////
+
+async function fetchAllBlogs(data = {}) {
+
+	const url = 'http://139.59.62.18:2018/blog/fetchAll'
+
+	const response = await fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	});
+	return response.json();
+}
+
+function createListItems(items){
+
+	fetchAllBlogs()
+	.then((items) => {
+		var list = document.getElementById("outer-list");
+		for(var i = 0; i < items.length; i++) {
+
+			// A Tag //
+			var link = document.createElement("a");
+			link.href = `?name=${items[i].name}`;
+
+			// List item //
+			var listItem = document.createElement("div");
+			listItem.id = "list-item";
+
+			// Title //
+			var title = document.createElement("p");
+			title.id = "list-item-title";
+			title.innerHTML = items[i].title;
+			listItem.appendChild(title);
+
+			// Time //
+			var time = document.createElement("p");
+			time.id = "list-item-time";
+			time.innerHTML = new Date(items[i].createdAt).toDateString();
+			listItem.appendChild(time);
+
+			link.appendChild(listItem);
+			list.appendChild(link);
+		}
+	})
+	.catch(console.log)
 }
