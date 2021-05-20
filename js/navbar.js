47fd7b88577f3ed00ca58818d35571813f9ae014
thatskevinjain <thatskevinjain@gmail.com>
2021-05-20 14:12:04 +0530

    Improved layout for bigger screens, removed navbar persistence

--- js/navbar.js
+++ js/navbar.js
@@ -1,20 +1,20 @@
-// When the user scrolls the page, execute stickNavbarOnTop //
-window.onscroll = function(){
-	stickNavbarOnTop()
-};
+// // When the user scrolls the page, execute stickNavbarOnTop //
+// window.onscroll = function(){
+// 	stickNavbarOnTop()
+// };
 
-// Get the navbar //
-var navbar = document.getElementById("navbar");
+// // Get the navbar //
+// var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position //
-// Remove "sticky" when you leave the scroll position //
-function stickNavbarOnTop() {
-	if (window.pageYOffset >= sticky) {
-		navbar.classList.add("sticky");
-	} else {
-		navbar.classList.remove("sticky");
-	}
-}
+// // Add the sticky class to the navbar when you reach its scroll position //
+// // Remove "sticky" when you leave the scroll position //
+// function stickNavbarOnTop() {
+// 	if (window.pageYOffset >= sticky) {
+// 		navbar.classList.add("sticky");
+// 	} else {
+// 		navbar.classList.remove("sticky");
+// 	}
+// }
